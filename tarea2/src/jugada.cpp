db18fd821e4d33696eeb133bfe7ba8d4740337d1
Matilde García <138676242+matildegarcia@users.noreply.github.com>
2023-09-11 10:35:04 -0300

    Update jugada.cpp

--- tarea2/src/jugada.cpp
+++ tarea2/src/jugada.cpp
@@ -2,20 +2,19 @@
 
 const char *obtenerNombreMovimiento(int numero);
 
-// Define la estructura Jugada
 struct rep_jugada
 {
     int numero;
     Movimiento movimientos[4];
 };
 
-// Crea una jugada con los valores dados
+
 TJugada crearTJugada(int numero, Movimiento mov1, Movimiento mov2, Movimiento mov3, Movimiento mov4)
 {
 
-    TJugada nuevaJugada = new rep_jugada; // Crear una nueva jugada en memoria dinámica
+    TJugada nuevaJugada = new rep_jugada; 
 
-    nuevaJugada->numero = numero; // Asignar el número de la jugada
+    nuevaJugada->numero = numero; 
 
     // Asignar los movimientos a la jugada
     nuevaJugada->movimientos[0] = mov1;
@@ -26,7 +25,7 @@ TJugada crearTJugada(int numero, Movimiento mov1, Movimiento mov2, Movimiento mo
     return nuevaJugada;
 }
 
-// Función que retorna el numero de la jugada
+
 int numeroTJugada(TJugada jugada)
 {
     int devuelve = 0;
@@ -37,21 +36,20 @@ int numeroTJugada(TJugada jugada)
     return devuelve;
 }
 
-// Función que retorna los movimientos de una jugada
+
 Movimiento *movimientoTJugada(TJugada jugada)
 {
 
     return jugada->movimientos;
 }
 
-// Devuelve true si y solo si la jugada tiene el movimiento mov en la posicion pos
 bool tieneMovimientoEnPosTJugada(TJugada jugada, int pos, Movimiento mov)
 {
     if (pos >= 0 && pos < 4)
     { // Asegurarse de que la posición esté en el rango del array
         return jugada->movimientos[pos] == mov;
     }
-    return false; // Si la posición está fuera del rango, retornar false
+    return false; 
 }
 
 const char *obtenerNombreMovimiento(int numero)
@@ -85,12 +83,11 @@ void liberarTJugada(TJugada &jugada)
 {
     if (jugada != NULL)
     {
-        delete jugada; // Liberar memoria del puntero a la estructura jugada
-        jugada = NULL; // Establecer el puntero a NULL
+        delete jugada; 
+        jugada = NULL; 
     }
 }
 
-// Función que crea una copia de la jugada, incluyendo copias de los movimientos
 TJugada copiarTJugada(TJugada jugada)
 {
     if (jugada == NULL)
@@ -98,11 +95,10 @@ TJugada copiarTJugada(TJugada jugada)
         return NULL;
     }
 
-    TJugada nuevaJugada = new rep_jugada; // Crear una nueva jugada en memoria dinámica
+    TJugada nuevaJugada = new rep_jugada;
 
-    nuevaJugada->numero = jugada->numero; // Copiar el número de la jugada
+    nuevaJugada->numero = jugada->numero;
 
-    // Copiar los movimientos a la nueva jugada
     for (int i = 0; i < 4; i++)
     {
         nuevaJugada->movimientos[i] = jugada->movimientos[i];
